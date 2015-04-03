868c24d9dbe205c7979e4a2a34960f5383deccde
Yader Velasquez <yajosev@gmail.com>
2015-04-03 16:22:21 -0600

    inicio de interfaz

--- js/activity.js
+++ js/activity.js
@@ -1,10 +1,7 @@
 define(function (require) {
     var activity = require("sugar-web/activity/activity");
 
-    // Manipulate the DOM only when it is ready.
     require(['domReady!'], function (doc) {
-
-        // Initialize the activity.
         activity.setup();
 
     });
