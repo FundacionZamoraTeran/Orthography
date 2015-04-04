44a0d321094982bf22e3b9f718f2b5c3aa546326
Yader Velasquez <yajosev@gmail.com>
2015-04-03 18:02:13 -0600

    Sugar imcompatible con bootstrap -.-

--- js/activity.js
+++ js/activity.js
@@ -1,9 +1,11 @@
 define(function (require) {
     var activity = require("sugar-web/activity/activity");
+    var icon = require("sugar-web/graphics/icon");
+    var l10n = require("webL10n");
 
     require(['domReady!'], function (doc) {
         activity.setup();
-
+        console.log("Hello World");
     });
 
 });
