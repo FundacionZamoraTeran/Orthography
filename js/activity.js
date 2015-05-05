95949fa4a9b7505f9eacbfd1be011779cfbe407b
Yader Velasquez <yajosev@gmail.com>
2015-05-04 21:31:50 -0600

    Nuevo diseño

--- js/activity.js
+++ js/activity.js
@@ -6,10 +6,38 @@ define(function (require) {
 
     require(['domReady!'], function (doc) {
         activity.setup();
+        // Cuadro de juego
         var description_game = document.getElementById('description-game');
         description_game.style.left = String((window.innerWidth / 2) - (description_game.offsetWidth / 2)) + "px";
-        description_game.style.top = String((window.innerHeight / 2) - (description_game.offsetHeight)) + "px";
+        description_game.style.top = String((window.innerHeight / 3) - (description_game.offsetHeight)) + "px";
 
+        var listWords = [
+            {
+                'sentence': 'La ___ es bonita',
+                'op1': 'caza',
+                'op2': 'casa',
+                'op1_concepto': 'Verbo',
+                'op2_concepto': 'Sustantivo',
+                'answer': 'op2',
+            },
+            {
+                'sentence': 'Ya le puse el ______ a la caña de pescar.',
+                'op1': 'cebo',
+                'op2': 'sebo',
+                'op1_concepto': 'Comida o cosas que simulan serlo',
+                'op2_concepto': 'Grasa sólida y dura que se extrae de algunos animales',
+                'answer': 'op1',
+            },
+        ];
+        word = listWords[Math.floor(Math.random() * (listWords.length))];
+        document.getElementById('sentence').innerHTML = word.sentence;
+        document.getElementById('op1').innerHTML = word.op1;
+        document.getElementById('op2').innerHTML = word.op2;
+
+
+
+
+        // Barra superior
         var optionButton = document.getElementById("option-button");
         var optionPallete = new palette.Palette(optionButton, "Opciones");
         var selects = document.createElement("div");
